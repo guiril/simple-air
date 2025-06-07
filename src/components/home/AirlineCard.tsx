36990f3b5bf539bab42775567747e6cc4d0ed89b
Ya-Jhao Chen <guirilchen@gmail.com>
2025-06-07 16:23:48 +0800

    Restructure components

--- src/components/home/AirlineCard.tsx
+++ src/components/home/AirlineCard.tsx
@@ -10,56 +10,6 @@ interface AirlineCardProps {
   index: number;
 }
 
-// export default function AirlineCard() {
-//   return itineraries.map((itinerary, index) => {
-//     return (
-//       <li
-//         key={itinerary.imgTitle}
-//         className={`flex flex-col py-[15px] px-4 bg-neutrail-0 border border-neutral-700/[.12] sm:mb-4 sm:last:mb-0 rounded-xl ${
-//           index === 2 || index === 3 ? 'md:hidden sm:flex' : ''
-//         }`}
-//       >
-//         <div className="mb-4 flex items-center">
-//           <Image
-//             src={`/images/airlines/${itinerary.imgTitle}.webp`}
-//             alt={itinerary.imgTitle}
-//             width={64}
-//             height={64}
-//             className="mr-3 rounded lg:shrink"
-//           />
-//           <div className="flex flex-col">
-//             <div className="flex items-center mb-2 text-p-md-sb text-neutral-700">
-//               <span>{itinerary.fromPlace}</span>
-//               <Image
-//                 src={'/images/icons/round-arrow.svg'}
-//                 alt={'round trip arrow'}
-//                 width={20}
-//                 height={20}
-//                 className="mx-2"
-//               />
-//               <span>{itinerary.toPlace}</span>
-//             </div>
-//             <span className="mb-1 text-p-xs text-neutral-500">
-//               {itinerary.period}
-//             </span>
-//             <span className="text-p-xs text-neutral-500 sm:hidden">
-//               {itinerary.cabin}
-//             </span>
-//           </div>
-//         </div>
-//         <div className="self-end">
-//           <span className="mr-2 text-p-xs text-neutral-500 sm:hidden">
-//             來回票價
-//           </span>
-//           <span className="text-p-md-sb text-primary-500">
-//             NT${itinerary.price}
-//           </span>
-//         </div>
-//       </li>
-//     );
-//   });
-// }
-
 export default function AirlineCard({
   imgTitle,
   fromPlace,
@@ -69,6 +19,51 @@ export default function AirlineCard({
   price,
   index
 }: AirlineCardProps) {
+  const imageEl = (
+    <Image
+      src={`/images/airlines/${imgTitle}.webp`}
+      alt={imgTitle}
+      width={64}
+      height={64}
+      className="mr-3 rounded lg:shrink"
+    />
+  );
+
+  const arrowEl = (
+    <Image
+      src={'/images/icons/round-arrow.svg'}
+      alt={'round trip arrow'}
+      width={20}
+      height={20}
+      className="mx-2"
+    />
+  );
+
+  const placeEl = (
+    <div className="flex items-center mb-2 text-p-md-sb text-neutral-700">
+      <span>{fromPlace}</span>
+      {arrowEl}
+      <span>{toPlace}</span>
+    </div>
+  );
+
+  const periodEl = (
+    <span className="mb-1 text-p-xs text-neutral-500">{period}</span>
+  );
+
+  const cabinEl = (
+    <span className="text-p-xs text-neutral-500 sm:hidden">{cabin}</span>
+  );
+
+  const priceEl = (
+    <>
+      <span className="mr-2 text-p-xs text-neutral-500 sm:hidden">
+        來回票價
+      </span>
+      <span className="text-p-md-sb text-primary-500">{`NT$${price}`}</span>
+    </>
+  );
+
   return (
     <li
       key={imgTitle}
@@ -77,35 +72,14 @@ export default function AirlineCard({
       }`}
     >
       <div className="mb-4 flex items-center">
-        <Image
-          src={`/images/airlines/${imgTitle}.webp`}
-          alt={imgTitle}
-          width={64}
-          height={64}
-          className="mr-3 rounded lg:shrink"
-        />
+        {imageEl}
         <div className="flex flex-col">
-          <div className="flex items-center mb-2 text-p-md-sb text-neutral-700">
-            <span>{fromPlace}</span>
-            <Image
-              src={'/images/icons/round-arrow.svg'}
-              alt={'round trip arrow'}
-              width={20}
-              height={20}
-              className="mx-2"
-            />
-            <span>{toPlace}</span>
-          </div>
-          <span className="mb-1 text-p-xs text-neutral-500">{period}</span>
-          <span className="text-p-xs text-neutral-500 sm:hidden">{cabin}</span>
+          {placeEl}
+          {periodEl}
+          {cabinEl}
         </div>
       </div>
-      <div className="self-end">
-        <span className="mr-2 text-p-xs text-neutral-500 sm:hidden">
-          來回票價
-        </span>
-        <span className="text-p-md-sb text-primary-500">{`NT$${price}`}</span>
-      </div>
+      <div className="self-end">{priceEl}</div>
     </li>
   );
 }
--- src/components/home/ItineraryCard/ImageCard.tsx
+++ src/components/home/ItineraryCard/ImageCard.tsx
@@ -8,45 +8,59 @@ export default function ImageCard({
   location,
   price
 }: ProductInfo) {
+  const labelTextEl = labelText && (
+    <span
+      className="absolute top-3 right-3 py-1 px-2
+        text-p-xs text-neutral-700 bg-neutral-0 rounded"
+    >
+      {labelText}
+    </span>
+  );
+
+  const imageEl = (
+    <Image
+      src={`/images/${imgTitle}.webp`}
+      alt={imgTitle}
+      width={410}
+      height={348}
+      className="rounded-xl w-full"
+    />
+  );
+
+  const remaingNumberEl = remaingNumber && (
+    <span
+      className="absolute bottom-3 left-3 py-1 px-2
+        text-p-xs text-neutral-100 bg-[#0AA4E7] rounded"
+    >
+      {`剩 ${remaingNumber} 張`}
+    </span>
+  );
+
+  const locationEl = (
+    <span className="text-title-md-sb text-neutral-700">{location}</span>
+  );
+
+  const priceEl = (
+    <span className="text-title-md-sb text-primary-500">{`NT$ ${price}`}</span>
+  );
+
   return (
     <li
       key={location}
       className="md:last:hidden sm:last:block sm:mb-8 sm:last:mb-0"
     >
       <div className="relative mb-6 sm:mb-[10px]">
-        {labelText && (
-          <span
-            className="absolute top-3 right-3 py-1 px-2
-              text-p-xs text-neutral-700 bg-neutral-0 rounded"
-          >
-            {labelText}
-          </span>
-        )}
-        <Image
-          src={`/images/${imgTitle}.webp`}
-          alt={imgTitle}
-          width={410}
-          height={348}
-          className="rounded-xl w-full"
-        />
-        {typeof remaingNumber === 'number' && (
-          <span
-            className="absolute bottom-3 left-3 py-1 px-2
-              text-p-xs text-neutral-100 bg-[#0AA4E7] rounded"
-          >
-            剩 {remaingNumber} 張
-          </span>
-        )}
+        {labelTextEl}
+        {imageEl}
+        {remaingNumberEl}
       </div>
       <div className="flex justify-between items-center">
-        <span className="text-title-md-sb text-neutral-700">{location}</span>
+        {locationEl}
         <div className="flex items-center">
           <span className="mr-3 text-p-sm-r-2 text-neutral-500 sm:hidden">
             最低價
           </span>
-          <span className="text-title-md-sb text-primary-500">
-            {`NT$ ${price}`}
-          </span>
+          {priceEl}
         </div>
       </div>
     </li>
